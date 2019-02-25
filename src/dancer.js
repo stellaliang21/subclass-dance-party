3ac44f889f2ebfb4ccff8ea31feb76f8cd593933
stellaliang21 <stellaliang2014@gmail.com>
2019-02-25 15:46:06 -0800

    Fully changed dancer from functional to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -7,22 +7,8 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  this.step();
-
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
@@ -30,6 +16,21 @@ makeDancer.prototype.step = function() {
   // it just schedules the next step
   setTimeout(this.step, this.timeBetweenSteps);
 };
+
+this.step();
+
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.css(styleSettings);
+};
+
+this.setPosition(top, left);
 
 // var dancer = {};
 
