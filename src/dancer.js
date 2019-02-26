4a7767133185b802100dff0bdd995866ff1180d1
stellaliang21 <stellaliang2014@gmail.com>
2019-02-25 21:56:21 -0800

    Refactored so our code now creates red blinks
    We were trying to call "step" before we set "$node," which step needs
    to run

--- src/dancer.js
+++ src/dancer.js
@@ -4,9 +4,8 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
-  console.log('this'+ this);
-  this.step();
   this.$node = $('<span class="dancer"></span>');
+  this.step();
   this.setPosition(top, left);
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
@@ -26,7 +25,6 @@ makeDancer.prototype.step = function() {
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
-  //
   var styleSettings = {
     top: this.top,
     left: this.left
